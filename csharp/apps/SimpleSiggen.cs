fb7addd7892e37f14fff3432fbb22fe5552a16c5
Nicholas Corgan <n.corgan@gmail.com>
2021-12-12 10:39:45 -0600

    Implemented options

--- csharp/apps/SimpleSiggen.cs
+++ csharp/apps/SimpleSiggen.cs
@@ -3,11 +3,20 @@
 
 using System;
 using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Invocation;
+using System.CommandLine.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.ExceptionServices;
 using System.Threading.Tasks;
 
 public class SimpleSiggen
 {
+    //
+    // Utility code
+    //
+
     // https://stackoverflow.com/a/67131017
     private static float[] LinSpace(float startval, float endval, int steps)
     {
@@ -34,7 +43,11 @@ public class SimpleSiggen
         System.Console.WriteLine("{0}: {1}: {2}", System.DateTime.Now, logLevel, message);
     }
 
-    private static void SiggenApp(
+    //
+    // Actual app logic
+    //
+
+    private static int SiggenApp(
         string args,
         double rate,
         float ampl,
@@ -47,122 +60,176 @@ public class SimpleSiggen
         double waveFreq,
         bool debug)
     {
-        SoapySDR.Logger.RegisterLogHandler(LogFunction);
-        SoapySDR.Logger.LogLevel = debug ? SoapySDR.LogLevel.Debug : SoapySDR.LogLevel.Warning;
-
-        var sdr = new SoapySDR.Device(args);
-
-        System.Console.WriteLine("Setting master clock rate to {0} MHz", clockRate / 1e6);
-        sdr.MasterClockRate = clockRate;
-        System.Console.WriteLine("Actual master clock rate: {0} MHz\n", sdr.MasterClockRate / 1e6);
-
-        System.Console.WriteLine("Setting Tx rate to {0} Msps", rate);
-        sdr.SetSampleRate(SoapySDR.Direction.Tx, txChan, rate);
-        System.Console.WriteLine("Actual Tx Rate {0} Msps\n", sdr.GetSampleRate(SoapySDR.Direction.Tx, txChan) / 1e6);
-
-        System.Console.WriteLine("Setting Tx bandwidth to {0} MHz", txBandwidth / 1e6);
-        sdr.SetBandwidth(SoapySDR.Direction.Tx, txChan, txBandwidth);
-        System.Console.WriteLine("Actual Tx bandwidth: {0} MHz\n", sdr.GetBandwidth(SoapySDR.Direction.Tx, txChan) / 1e6);
-
-        System.Console.WriteLine("Setting Tx antenna to {0}\n", txAntenna);
-        sdr.SetAntenna(SoapySDR.Direction.Tx, txChan, txAntenna);
-
-        System.Console.WriteLine("Setting Tx gain to {0} dB", txGain);
-        sdr.SetGain(SoapySDR.Direction.Tx, txChan, txGain);
-        System.Console.WriteLine("Actual Tx gain: {0} dB\n", sdr.GetGain(SoapySDR.Direction.Tx, txChan));
-
-        System.Console.WriteLine("Tuning the frontend to {0} MHz", freq);
-        sdr.SetFrequency(SoapySDR.Direction.Tx, txChan, freq);
-        System.Console.WriteLine("Actual Tx frequency: {0} MHz\n", sdr.GetFrequency(SoapySDR.Direction.Tx, txChan));
-
-        System.Console.WriteLine("Create Tx stream");
-        var txStream = sdr.SetupTxStream(SoapySDR.StreamFormat.CF32, new uint[] { txChan }, "");
-        System.Console.WriteLine("Activate Tx stream");
-        txStream.Activate(SoapySDR.StreamFlags.None);
-
-        var mtu = txStream.MTU;
-        System.Console.WriteLine("Stream MTU: {0}", mtu);
-
-        var phaseAcc = 0.0F;
-        var phaseInc = 2.0F * Math.PI * waveFreq / rate;
-
-        uint totalSamps = 0;
-        bool running = true;
-
-        // Handle Ctrl+C
-        System.Console.CancelKeyPress += delegate (object sender, ConsoleCancelEventArgs e)
+        try
         {
-            e.Cancel = true;
-            running = false;
-        };
+            SoapySDR.Logger.RegisterLogHandler(LogFunction);
+            SoapySDR.Logger.LogLevel = debug ? SoapySDR.LogLevel.Debug : SoapySDR.LogLevel.Warning;
 
-        var lastPrintTime = System.DateTime.Now;
+            System.Console.WriteLine("Creating SDR with args: \"{0}\"", args);
+            var sdr = new SoapySDR.Device(args);
+            System.Console.WriteLine("Found {0}\n", sdr);
 
-        while(running)
-        {
-            float phaseAccNext = (float)(phaseAcc + (mtu * phaseInc));
-            var phases = LinSpace(phaseAcc, phaseAccNext, (int)mtu);
-            var samples = MakeSamples(ampl, phases);
-
-            phaseAcc = phaseAccNext;
-            while (phaseAcc > (Math.PI * 2.0F)) phaseAcc -= (float)(Math.PI * 2.0F);
-
-            var streamResult = new SoapySDR.StreamResult();
-            var status = txStream.Write(samples, 0, 1000000, out streamResult);
-            if (status != SoapySDR.ErrorCode.None)
+            if(clockRate > 0)
             {
-                throw new ApplicationException(string.Format("Write returned: {0}", status));
+                System.Console.WriteLine("Setting master clock rate to {0} MHz", clockRate / 1e6);
+                sdr.MasterClockRate = clockRate;
             }
-            else if (streamResult.NumSamples != mtu)
+            System.Console.WriteLine("Actual master clock rate: {0} MHz\n", sdr.MasterClockRate / 1e6);
+
+            if(rate > 0)
             {
-                throw new ApplicationException(string.Format("Write consumed {0} elements, expected {1}", streamResult.NumSamples, mtu));
+                System.Console.WriteLine("Setting Tx rate to {0} Msps", rate / 1e6);
+                sdr.SetSampleRate(SoapySDR.Direction.Tx, txChan, rate);
             }
+            System.Console.WriteLine("Actual Tx Rate {0} Msps\n", sdr.GetSampleRate(SoapySDR.Direction.Tx, txChan) / 1e6);
 
-            totalSamps += streamResult.NumSamples;
-
-            // Print rate every 5 seconds
-            var now = System.DateTime.Now;
-            var durationSecs = (System.DateTime.Now - lastPrintTime).TotalSeconds;
-            if (durationSecs > 5)
+            if(txBandwidth > 0)
             {
-                var currentRate = totalSamps / durationSecs / 1e6;
-                System.Console.WriteLine("C# siggen rate: {0} Msps", currentRate);
-
-                totalSamps = 0;
-                lastPrintTime = System.DateTime.Now;
+                System.Console.WriteLine("Setting Tx bandwidth to {0} MHz", txBandwidth / 1e6);
+                sdr.SetBandwidth(SoapySDR.Direction.Tx, txChan, txBandwidth);
             }
+            System.Console.WriteLine("Actual Tx bandwidth: {0} MHz\n", sdr.GetBandwidth(SoapySDR.Direction.Tx, txChan) / 1e6);
+
+            if(txAntenna.Length > 0)
+            {
+                System.Console.WriteLine("Setting Tx antenna to {0}\n", txAntenna);
+                sdr.SetAntenna(SoapySDR.Direction.Tx, txChan, txAntenna);
+            }
+            System.Console.WriteLine("Actual Tx antenna: {0}\n", sdr.GetAntenna(SoapySDR.Direction.Tx, txChan));
+
+            if(txGain > 0)
+            {
+                System.Console.WriteLine("Setting Tx gain to {0} dB", txGain);
+                sdr.SetGain(SoapySDR.Direction.Tx, txChan, txGain);
+            }
+            System.Console.WriteLine("Actual Tx gain: {0} dB\n", sdr.GetGain(SoapySDR.Direction.Tx, txChan));
+
+            if(freq > 0.0)
+            {
+                System.Console.WriteLine("Tuning the frontend to {0} MHz", freq);
+                sdr.SetFrequency(SoapySDR.Direction.Tx, txChan, freq);
+                System.Console.WriteLine("Actual Tx frequency: {0} MHz\n", sdr.GetFrequency(SoapySDR.Direction.Tx, txChan));
+            }
+
+            var format = SoapySDR.StreamFormat.CF32;
+            System.Console.WriteLine(string.Format("Create Tx stream (format: {0}, chan: {1})", format, txChan));
+            var txStream = sdr.SetupTxStream(format, new uint[] { txChan }, "");
+            System.Console.WriteLine("Activate Tx stream");
+            txStream.Activate(SoapySDR.StreamFlags.None);
+
+            var mtu = txStream.MTU;
+            System.Console.WriteLine("Stream MTU: {0}", mtu);
+
+            var actualWaveFreq = (waveFreq > 0.0) ? waveFreq : (rate / 10.0);
+
+            var phaseAcc = 0.0F;
+            var phaseInc = 2.0F * Math.PI * actualWaveFreq / rate;
+
+            uint totalSamps = 0;
+            bool running = true;
+
+            // Handle Ctrl+C
+            System.Console.CancelKeyPress += delegate (object sender, ConsoleCancelEventArgs e)
+            {
+                e.Cancel = true;
+                running = false;
+            };
+
+            var lastPrintTime = System.DateTime.Now;
+
+            System.Console.WriteLine("\nStarting...");
+            while (running)
+            {
+                float phaseAccNext = (float)(phaseAcc + (mtu * phaseInc));
+                var phases = LinSpace(phaseAcc, phaseAccNext, (int)mtu);
+                var samples = MakeSamples(ampl, phases);
+
+                phaseAcc = phaseAccNext;
+                while (phaseAcc > (Math.PI * 2.0F)) phaseAcc -= (float)(Math.PI * 2.0F);
+
+                var streamResult = new SoapySDR.StreamResult();
+                var status = txStream.Write(samples, 0, 1000000, out streamResult);
+                if (status != SoapySDR.ErrorCode.None)
+                {
+                    throw new ApplicationException(string.Format("Write returned: {0}", status));
+                }
+                else if (streamResult.NumSamples != mtu)
+                {
+                    throw new ApplicationException(string.Format("Write consumed {0} elements, expected {1}", streamResult.NumSamples, mtu));
+                }
+
+                totalSamps += streamResult.NumSamples;
+
+                // Print rate every 5 seconds
+                var now = System.DateTime.Now;
+                var durationSecs = (System.DateTime.Now - lastPrintTime).TotalSeconds;
+                if (durationSecs > 5)
+                {
+                    var currentRate = totalSamps / durationSecs / 1e6;
+                    System.Console.WriteLine("C# siggen rate: {0} Msps", currentRate);
+
+                    totalSamps = 0;
+                    lastPrintTime = System.DateTime.Now;
+                }
+            }
+
+            // Executed after Ctrl+C
+            System.Console.WriteLine("Clean up stream");
+            txStream.Deactivate();
+            txStream.Close();
+            System.Console.WriteLine("Done!");
+
+            return 0;
         }
-
-        // Executed after Ctrl+C
-        System.Console.WriteLine("Clean up stream");
-        txStream.Deactivate();
-        txStream.Close();
-        System.Console.WriteLine("Done!");
+        catch(Exception ex)
+        {
+            System.Console.Error.WriteLine(ex);
+            return -1;
+        }
     }
 
-    public static async Task<int> Main(string[] args)
+    public static int Main(string[] args)
     {
         /*
-        parser.add_argument("--args", type=str, help="device factor arguments", default="")
-        parser.add_argument("--rate", type=float, help="Tx and Rx sample rate", default=1e6)
-        parser.add_argument("--ampl", type=float, help="Tx digital amplitude rate", default=0.7)
-        parser.add_argument("--tx-ant", type=str, help="Optional Tx antenna")
-        parser.add_argument("--tx-gain", type=float, help="Optional Tx gain (dB)")
-        parser.add_argument("--tx-chan", type=int, help="Transmitter channel (def=0)", default=0)
-        parser.add_argument("--freq", type=float, help="Optional Tx and Rx freq (Hz)")
-        parser.add_argument("--tx-bw", type=float, help="Optional Tx filter bw (Hz)", default=5e6)
-        parser.add_argument("--wave-freq", type=float, help="Baseband waveform freq (Hz)")
-        parser.add_argument("--clock-rate", type=float, help="Optional clock rate (Hz)")
-        parser.add_argument("--debug", action='store_true', help="Output debug messages")
-        parser.add_argument(
-            "--abort-on-error", action='store_true',
-            help="Halts operations if the SDR logs an error")
+         * Unintuitively, "IsRequired = true" refers to whether a value is required when the option is passed in,
+         * *not* if the option itself is required. Options themselves become required if there is no default value.
+         * Since these types can't be nullable in the C# standard we're targeting, we'll just need to use a default
+         * value and check for it in the function itself.
          */
+        var rootCmd = new RootCommand() { TreatUnmatchedTokensAsErrors = true }.WithHandler(nameof(SiggenApp));
+        rootCmd.Description = "A simple signal generator example to demonstrate the SoapySDR C# API.\n\nNote: " +
+                              "(REQUIRED) in the option descriptions below refers to a value being necessary " +
+                              "for the option, not the option itself being required.";
 
-        RootCommand rootCommand = new RootCommand(
-            description: "Simple signal generator for testing transmit\n" +
-            "Continuously output a carrier with single sideband sinusoid amplitude modulation.");
+        rootCmd.AddOption(new Option<string>("--args", description: "Device args") { IsRequired = true });
+        rootCmd.AddOption(new Option<double>("--rate", description: "Tx sample rate (samples/sec)", getDefaultValue: () => 1e6) { IsRequired = true });
+        rootCmd.AddOption(new Option<double>("--ampl", description: "Optional Tx digital amplitude rate (0.0-1.0)", getDefaultValue: () => 0.7) { IsRequired = true });
+        rootCmd.AddOption(new Option<double>("--freq", description: "Tx frequency (Hz)") { IsRequired = true });
+        rootCmd.AddOption(new Option<double>("--bw", description: "Optional Tx filter bandwidth (Hz)", getDefaultValue: () => 5e6) { IsRequired = true });
+        rootCmd.AddOption(new Option<uint>("--chan", description: "Optional Tx channel", getDefaultValue: () => 0U) { IsRequired = true });
+        rootCmd.AddOption(new Option<double>("--gain", description: "Optional Tx gain", getDefaultValue: () => 0.0) { IsRequired = true });
+        rootCmd.AddOption(new Option<string>("--ant", description: "Optional Tx antenna", getDefaultValue: () => "") { IsRequired = true });
+        rootCmd.AddOption(new Option<double>("--clock-rate", description: "Optional Tx clock rate (Hz)", getDefaultValue: () => 0.0) { IsRequired = true });
+        rootCmd.AddOption(new Option<double>("--wave-freq", description: "Optional baseband waveform freq (Hz), actually defaults to (rate / 10)", getDefaultValue: () => 0.0));
+        rootCmd.AddOption(new Option<bool>("--debug", description: "Log debug info", getDefaultValue: () => false) { IsRequired = true });
 
-        return await rootCommand.InvokeAsync(args);
+        // Note: exceptions must be caught in the function itself, or System.CommandLine will intercept it and print
+        // the entire stacktrace, which includes a bunch of dynamic invoking stuff we don't care about.
+        return rootCmd.Invoke(args);
+    }
+}
+
+//
+// Convenient System.CommandLine extensions
+//
+
+static class CommandLineHelpers
+{
+    public static RootCommand WithHandler(this RootCommand command, string methodName)
+    {
+        var method = typeof(SimpleSiggen).GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Static);
+        var handler = CommandHandler.Create(method);
+        command.Handler = handler;
+        return command;
     }
 }
