e82074edb1ea51341cc05b21f8cc46ed407a602d
Nicholas Corgan <n.corgan@gmail.com>
2022-04-03 14:32:00 -0500

    Added TestEnumerateDevices, broken from likely SWIG bug

---
+++ swig/octave/tests/TestEnumerateDevices.m
@@ -0,0 +1,19 @@
+# Copyright (c) 2022 Nicholas Corgan
+# SPDX-License-Identifier: BSL-1.0
+
+# Not a function file
+1;
+
+%!test
+%! SoapySDR
+%! #TODO: figure out SWIG printing error, making test fail
+%!
+%! # Just make sure this runs, no idea what's actually there
+%! deviceNoArgs = SoapySDR.Device.enumerate()
+%!
+%! #TODO: check output for null device
+%! deviceStringArgs = SoapySDR.Device.enumerate("type=null")
+%!
+%! #TODO: check output for null device
+%! argsMap = container.Map({"type"}, {"null"})
+%! deviceMapArgs = SoapySDR.Device.enumerate(argsMap)
