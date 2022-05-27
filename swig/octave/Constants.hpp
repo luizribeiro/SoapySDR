4eed49934e57a2c6d7982a9613110bdbd240a3d2
Nicholas Corgan <n.corgan@gmail.com>
2022-05-27 18:11:07 -0500

    Error code and format tests

--- swig/octave/Constants.hpp
+++ swig/octave/Constants.hpp
@@ -5,7 +5,7 @@
 
 #include <SoapySDR/Constants.h>
 #include <SoapySDR/Errors.hpp>
-#include <SoapySDR/Formats.h>
+#include <SoapySDR/Formats.hpp>
 #include <SoapySDR/Logger.hpp>
 #include <SoapySDR/Types.hpp>
 
@@ -126,11 +126,13 @@ struct StreamFormat
     static const std::string F32;
     static const std::string F64;
 
+    static const std::string CS4;
     static const std::string CS8;
     static const std::string CS12;
     static const std::string CS16;
     static const std::string CS32;
 
+    static const std::string CU4;
     static const std::string CU8;
     static const std::string CU12;
     static const std::string CU16;
@@ -138,6 +140,11 @@ struct StreamFormat
 
     static const std::string CF32;
     static const std::string CF64;
+
+    static inline size_t toSize(const std::string &format)
+    {
+        return SoapySDR::formatToSize(format);
+    }
 };
 
 const std::string StreamFormat::S8(SOAPY_SDR_S8);
@@ -151,11 +158,13 @@ const std::string StreamFormat::U32(SOAPY_SDR_U32);
 const std::string StreamFormat::F32(SOAPY_SDR_F32);
 const std::string StreamFormat::F64(SOAPY_SDR_F64);
 
+const std::string StreamFormat::CS4(SOAPY_SDR_CS4);
 const std::string StreamFormat::CS8(SOAPY_SDR_CS8);
 const std::string StreamFormat::CS12(SOAPY_SDR_CS12);
 const std::string StreamFormat::CS16(SOAPY_SDR_CS16);
 const std::string StreamFormat::CS32(SOAPY_SDR_CS32);
 
+const std::string StreamFormat::CU4(SOAPY_SDR_CU4);
 const std::string StreamFormat::CU8(SOAPY_SDR_CU8);
 const std::string StreamFormat::CU12(SOAPY_SDR_CU12);
 const std::string StreamFormat::CU16(SOAPY_SDR_CU16);
--- swig/octave/tests/TestErrorCodes.m
+++ swig/octave/tests/TestErrorCodes.m
@@ -8,10 +8,10 @@
 %! SoapySDR
 %!
 %! assert(SoapySDR.Error.toString(SoapySDR.Error.TIMEOUT) == "TIMEOUT")
-%! assert(SoapySDR.Error.toString(SoapySDR.Error.STREAM_ERROR), "STREAM_ERROR")
-%! assert(SoapySDR.Error.toString(SoapySDR.Error.CORRUPTION), "CORRUPTION")
-%! assert(SoapySDR.Error.toString(SoapySDR.Error.OVERFLOW), "OVERFLOW")
-%! assert(SoapySDR.Error.toString(SoapySDR.Error.NOT_SUPPORTED), "NOT_SUPPORTED")
-%! assert(SoapySDR.Error.toString(SoapySDR.Error.TIME_ERROR), "TIME_ERROR")
-%! assert(SoapySDR.Error.toString(SoapySDR.Error.UNDERFLOW), "UNDERFLOW")
-%! assert(SoapySDR.Error.toString(0), "UNKNOWN")
+%! assert(SoapySDR.Error.toString(SoapySDR.Error.STREAM_ERROR) == "STREAM_ERROR")
+%! assert(SoapySDR.Error.toString(SoapySDR.Error.CORRUPTION) == "CORRUPTION")
+%! assert(SoapySDR.Error.toString(SoapySDR.Error.OVERFLOW) == "OVERFLOW")
+%! assert(SoapySDR.Error.toString(SoapySDR.Error.NOT_SUPPORTED) == "NOT_SUPPORTED")
+%! assert(SoapySDR.Error.toString(SoapySDR.Error.TIME_ERROR) == "TIME_ERROR")
+%! assert(SoapySDR.Error.toString(SoapySDR.Error.UNDERFLOW) == "UNDERFLOW")
+%! assert(SoapySDR.Error.toString(0) == "UNKNOWN")
