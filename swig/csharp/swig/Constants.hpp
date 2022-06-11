53eba3b8b03d75b48f6d983d4ce081000eb38f6a
Nicholas Corgan <n.corgan@gmail.com>
2022-06-10 19:38:24 -0500

    Add missing enum validations

--- swig/csharp/swig/Constants.hpp
+++ swig/csharp/swig/Constants.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2020-2021 Nicholas Corgan
+// Copyright (c) 2020-2022 Nicholas Corgan
 // SPDX-License-Identifier: BSL-1.0
 
 #pragma once
@@ -57,3 +57,19 @@ namespace SoapySDR { namespace CSharp {
 
 ENUM_CHECK(SoapySDR::CSharp::Direction::Tx, SOAPY_SDR_TX);
 ENUM_CHECK(SoapySDR::CSharp::Direction::Rx, SOAPY_SDR_RX);
+ENUM_CHECK(SoapySDR::CSharp::ErrorCode::Timeout, SOAPY_SDR_TIMEOUT);
+ENUM_CHECK(SoapySDR::CSharp::ErrorCode::StreamError, SOAPY_SDR_STREAM_ERROR);
+ENUM_CHECK(SoapySDR::CSharp::ErrorCode::Corruption, SOAPY_SDR_CORRUPTION);
+ENUM_CHECK(SoapySDR::CSharp::ErrorCode::Overflow, SOAPY_SDR_OVERFLOW);
+ENUM_CHECK(SoapySDR::CSharp::ErrorCode::NotSupported, SOAPY_SDR_NOT_SUPPORTED);
+ENUM_CHECK(SoapySDR::CSharp::ErrorCode::TimeError, SOAPY_SDR_TIME_ERROR);
+ENUM_CHECK(SoapySDR::CSharp::ErrorCode::Underflow, SOAPY_SDR_UNDERFLOW);
+ENUM_CHECK(SoapySDR::CSharp::LogLevel::Fatal, SOAPY_SDR_FATAL);
+ENUM_CHECK(SoapySDR::CSharp::LogLevel::Critical, SOAPY_SDR_CRITICAL);
+ENUM_CHECK(SoapySDR::CSharp::LogLevel::Error, SOAPY_SDR_ERROR);
+ENUM_CHECK(SoapySDR::CSharp::LogLevel::Warning, SOAPY_SDR_WARNING);
+ENUM_CHECK(SoapySDR::CSharp::LogLevel::Notice, SOAPY_SDR_NOTICE);
+ENUM_CHECK(SoapySDR::CSharp::LogLevel::Info, SOAPY_SDR_INFO);
+ENUM_CHECK(SoapySDR::CSharp::LogLevel::Debug, SOAPY_SDR_DEBUG);
+ENUM_CHECK(SoapySDR::CSharp::LogLevel::Trace, SOAPY_SDR_TRACE);
+ENUM_CHECK(SoapySDR::CSharp::LogLevel::SSI, SOAPY_SDR_SSI);
